5abeeb6316c7775c2d4a1a1135478cc8bbe9f4d7
moepwintzune24@gmail.com <you@example.com>
2023-02-04 23:18:24 +0630

    first commit

--- src/Q10.java
+++ src/Q10.java
@@ -1,4 +1,7 @@
 public class Q10 {
     public static void main(String[] args) {
+        int numbers[]={5,4,1,8,6,9};
+        int valueNum=0;
+
     }
 }
--- src/Q14.java
+++ src/Q14.java
@@ -1,8 +1,5 @@
 public class Q14 {
     public static void main(String[] args) {
-        int randArray[]={1,2,5,6,7,10,7,8};
-        int sum=0;
 
-        for(int )
     }
 }
